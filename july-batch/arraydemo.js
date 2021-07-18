3d120770ba9f24a4b61f3a0fbff86a181b225166
Abhishek <abhi.ghosh27feb@gmail.com>
2021-07-18 17:52:17 +0530

    add

---
+++ july-batch/arraydemo.js
@@ -0,0 +1,14 @@
+//push pop shift unshift
+
+ const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.push("sweet lime")
+console.log("fruits",fruits)
+fruits.unshift("Sweet Lime")
+console.log("fruits",fruits)
+fruits.shift()
+console.log("fruits",fruits)
+fruits.splice(0, 3); 
+console.log("fruits",fruits)
+// delete fruits; 
+// console.log("fruits",fruits)
---
+++ july-batch/functiondemo.js
@@ -0,0 +1,35 @@
+// function defination
+// function hello(str){
+
+//     var a=0;
+//     let b=0;
+//     console.log("hello",str)
+// }
+// // calling
+// hello('abhishek',a,b)
+
+// // var let  es6 var es5
+
+// for (let i=0;i<10;i++){
+
+//     console.log("i==",i)
+// }
+// console.log("i==",i)
+
+// const i=0;
+
+// i=1;
+// console.log("i==",i)
+// hello()
+// function hello(){
+
+//     console.log("hello")
+// }
+
+// es6 fat arrow 
+// const  hello=(num)=>{
+//    return num ** 2
+//  }
+// madam
+
+// hello('abhishek')
