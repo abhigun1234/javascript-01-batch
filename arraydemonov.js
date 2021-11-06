de1f85105114d5398b240b2d6f436612900aa95f
Abhishek <abhi.ghosh27feb@gmail.com>
2021-11-06 12:55:45 +0530

    add

--- arraydemonov.js
+++ arraydemonov.js
@@ -10,3 +10,47 @@
 // console.log("fruits",fruits)
 // delete fruits; 
 // console.log("fruits",fruits)
+
+// function hello(str){
+
+//   console.log("hello",str)
+//   return 'hello abhishek'
+// }
+
+// var result=hello('abhishek')
+// console.log("RESULT",result)
+// es6
+// var hello=function(){
+
+//  console.log("hello")   
+// }
+// hello()
+
+// es6
+
+// const hello=(str)=>{
+//     console.log("hello")   
+//  return 'hhhh'
+// }
+
+// var gg=hello('bye')
+
+class Person{
+
+    constructor(_id,_name){
+   console.log("hello constructor")
+   this.id=_id;
+   this.name=_name
+    }
+    getPersonData(){
+
+        console.log("id",this.id)
+        console.log("name",this.name)
+    }
+}
+
+
+let p1=new Person(1,'abrar')
+p1.getPersonData()
+let p2=new Person(2,'abhishek')
+p1.getPersonData()
