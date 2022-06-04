276aa5ff33ded240dbe19abe842f8dabaccd5883
Abhishek <abhi.ghosh27feb@gmail.com>
2022-06-04 17:52:13 +0530

    add

---
+++ June-batch-2022/Firstday.js
@@ -0,0 +1,44 @@
+// console.log("hello world")
+// var a=0;
+// console.log("a",a)
+// var b;
+// console.log("b",b)
+// //data type
+// a='abhishek'
+// console.log("a",a)
+//operators 
+// arethemetic operator
+// conditional operator
+// logical operator
+//ternery operator
+
+// var no1=Number(prompt("enter first no"))
+// var no2=Number(prompt("enter second no"))
+// // additional operation
+// var result=no1+no2;
+// console.log("result",result)
+// conditional operator
+//<= >= == === != 
+// i='12';
+// j=12;
+// console.log("i==j",i==j)
+// console.log("i==j",i===j)
+// no1=prompt("enter first no")
+// no2=prompt("enter second no")
+// if(no1>no2){
+
+//     console.log("no1  greater")
+// }
+// else{
+//     console.log("no2 greater")
+// }
+// undifined null
+
+// var k;
+// var j=null;
+// console.log(typeof(k))
+// console.log(typeof(j))
+k=10;
+l=12;
+var result=k>l? "greter" : "lesser" 
+console.log("result",result)
