6d80487b3584f84f2cd582f9991fefe7ae70f693
Abhishek <abhi.ghosh27feb@gmail.com>
2022-07-08 20:51:25 +0530

    calc

---
+++ july-batch-2022/first.js
@@ -0,0 +1,27 @@
+console.log("hello")
+// variable   es5 
+var a=10
+console.log("a==",a)
+var a=true 
+console.log("a==",a)
+
+//  operators
+
+// arthemetic operator
+
+
+// + - / * **  
+// //es6 
+// let a=10;
+// a=true;
+// console.log("a")
+var a=Number(prompt("enter first no"))
+var b=Number(prompt("enter second no"))
+// add
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a**b)
+
+
