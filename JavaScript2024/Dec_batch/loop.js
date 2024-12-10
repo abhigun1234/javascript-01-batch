ecc0c4cf678c25f1e18dbe647d155da28ec576dc
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2024-12-10 21:03:22 +0530

    add

--- JavaScript2024/Dec_batch/loop.js
+++ JavaScript2024/Dec_batch/loop.js
@@ -1,1 +1,33 @@
-console.log("hello")
+// while do while for loop
+// var i=0
+//1
+// while(i<10){
+//     console.log("i",i)
+//     i++
+// }
+// // do while
+// var j=1
+// do{
+// console.log("j",j)
+// j++
+// }while(j<10)
+
+// //     // for 
+//     for (let i=0;i<10;i++){
+//         console.log("i",i)
+//     }
+//     console.log("i",i)
+    // var no =prompt("enter the no to print the table")
+    // var i=1;
+    // while (i<=10){
+    //     console.log("no ",no*i)
+    //     i++
+    // }
+    //enter a no and calculate the factorial of the no
+    //5=5*4*3*2*1
+    // 0 1 1 2 3 5 8 fabanocci series
+    // enter a no and check the no is prime or no
+
+    const k=10;
+    k=12
+    console.log("k=",k)
